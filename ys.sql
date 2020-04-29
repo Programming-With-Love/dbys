d4f99da4c9f464729ad9befe8d80f5b214372dfa
danbai225 <db225@qq.com>
2020-04-29 11:45:28 +0800

    表结构

--- ys.sql
+++ ys.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.15.10
--- https://www.phpmyadmin.net
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: 2019-11-23 12:37:09
--- 服务器版本： 5.6.46-log
--- PHP Version: 5.6.40
+-- 主机： localhost
+-- 生成日期： 2020-04-29 11:44:46
+-- 服务器版本： 5.5.62-log
+-- PHP 版本： 7.3.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ys`
+-- 数据库： `ys`
 --
 
 -- --------------------------------------------------------
@@ -26,10 +28,37 @@ SET time_zone = "+00:00";
 -- 表的结构 `config`
 --
 
-CREATE TABLE IF NOT EXISTS `config` (
+CREATE TABLE `config` (
   `Item` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '设置项名字',
   `value` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '值'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 转存表中的数据 `config`
+--
+
+INSERT INTO `config` (`Item`, `value`) VALUES
+('ad', ''),
+('appGG', '欢迎使用淡白影视,如遇问题下方反馈.'),
+('dmcache', 'no'),
+('footer', '<script> (function(){     var bp = document.createElement(\'script\');     var curProtocol = window.location.protocol.split(\':\')[0];     if (curProtocol === \'https\') {         bp.src = \'https://zz.bdstatic.com/linksubmit/push.js\';     }     else {         bp.src = \'http://push.zhanzhang.baidu.com/push.js\';     }     var s = document.getElementsByTagName(\"script\")[0];     s.parentNode.insertBefore(bp, s); })(); </script> <!-- Global site tag (gtag.js) - Google Analytics --> <script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-125430775-2\"></script> <script>   window.dataLayer = window.dataLayer || [];   function gtag(){dataLayer.push(arguments);}   gtag(\'js\', new Date());    gtag(\'config\', \'UA-125430775-2\'); \r\n</script>\r\n\r\n '),
+('gg', '欢迎使用淡白影视!<a href=\"http://cdn.p00q.cn/ys/app/%E6%B7%A1%E7%99%BD%E5%BD%B1%E8%A7%86V1.0.4.apk\">app下载</a>\r\n<a href=\"https://github.com/Programming-With-Love/dbys\">本网站开源地址</a> <span style=\"color:red\">切勿相信片中广告</span>'),
+('head', ''),
+('ylink', '[\r\n{name:\"淡白的博客\",url:\"https://p00q.cn\"},{name:\"MVCAT\",url:\"http://www.mvcat.com\"},\r\n{name:\"站点地图\",url:\"https://dbys.vip/sitemap.xml\"},\r\n{name:\"低价香港vps\",url:\"https://my.hostkvm.com/aff.php?aff=807\"},{name:\"临时云\",url:\"http://tempyun.com\"},{name:\"V聚焦\",url:\"http://www.vjujiao.com\"},{name:\"LOL坑逼网\",url:\"http://lolkb.top\"},{name:\"樱花动漫\",url:\"https://www.52xnet.cn\"},{name:\"舔狗日记\",url:\"http://tiangou.p00q.cn/\"},{name:\"广告联盟\",url:\"http://www.i8gg.com/cf.aspx?103\"}\r\n]');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id` int(128) NOT NULL,
+  `type` int(128) NOT NULL,
+  `content` text NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dispose` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='反馈表';
 
 -- --------------------------------------------------------
 
@@ -37,7 +66,7 @@ CREATE TABLE IF NOT EXISTS `config` (
 -- 表的结构 `tvb`
 --
 
-CREATE TABLE IF NOT EXISTS `tvb` (
+CREATE TABLE `tvb` (
   `id` int(16) NOT NULL COMMENT 'id',
   `name` varchar(1024) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '频道名',
   `url` varchar(1024) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '源路径',
@@ -51,7 +80,7 @@ CREATE TABLE IF NOT EXISTS `tvb` (
 -- 表的结构 `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `id` int(11) NOT NULL COMMENT '用户id',
   `username` varchar(32) COLLATE utf8_bin NOT NULL COMMENT '用户名',
   `user_type` int(11) NOT NULL DEFAULT '1' COMMENT '用户类型 0 禁用 1普通 2 管理员',
@@ -66,7 +95,8 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- 表的结构 `video_time`
 --
 
-CREATE TABLE IF NOT EXISTS `video_time` (
+CREATE TABLE `video_time` (
+  `id` bigint(20) NOT NULL,
   `username` varchar(32) COLLATE utf8_bin NOT NULL COMMENT '用户名',
   `ysid` int(11) NOT NULL,
   `ysjiname` varchar(128) COLLATE utf8_bin NOT NULL COMMENT '影视id',
@@ -80,7 +110,7 @@ CREATE TABLE IF NOT EXISTS `video_time` (
 -- 表的结构 `ysb`
 --
 
-CREATE TABLE IF NOT EXISTS `ysb` (
+CREATE TABLE `ysb` (
   `id` int(11) NOT NULL DEFAULT '0',
   `pm` text NOT NULL,
   `tp` text,
@@ -96,23 +126,29 @@ CREATE TABLE IF NOT EXISTS `ysb` (
   `pctime` text,
   `gxtime` text,
   `js` text,
-  `gkdz` text,
-  `xzdz` text
+  `gkdz` longblob,
+  `xzdz` longblob
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Indexes for dumped tables
+-- 转储表的索引
 --
 
 --
--- Indexes for table `config`
+-- 表的索引 `config`
 --
 ALTER TABLE `config`
   ADD UNIQUE KEY `Item_2` (`Item`),
   ADD KEY `Item` (`Item`);
 
 --
--- Indexes for table `tvb`
+-- 表的索引 `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 表的索引 `tvb`
 --
 ALTER TABLE `tvb`
   ADD PRIMARY KEY (`id`),
@@ -120,15 +156,22 @@ ALTER TABLE `tvb`
   ADD KEY `id` (`id`);
 
 --
--- Indexes for table `user`
+-- 表的索引 `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `username` (`username`),
   ADD KEY `id_2` (`id`);
 
 --
--- Indexes for table `ysb`
+-- 表的索引 `video_time`
+--
+ALTER TABLE `video_time`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 表的索引 `ysb`
 --
 ALTER TABLE `ysb`
   ADD PRIMARY KEY (`id`),
@@ -136,19 +179,34 @@ ALTER TABLE `ysb`
   ADD KEY `id_2` (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- 在导出的表使用AUTO_INCREMENT
 --
 
 --
--- AUTO_INCREMENT for table `tvb`
+-- 使用表AUTO_INCREMENT `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id` int(128) NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用表AUTO_INCREMENT `tvb`
 --
 ALTER TABLE `tvb`
   MODIFY `id` int(16) NOT NULL AUTO_INCREMENT COMMENT 'id';
+
 --
--- AUTO_INCREMENT for table `user`
+-- 使用表AUTO_INCREMENT `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id';
+
+--
+-- 使用表AUTO_INCREMENT `video_time`
+--
+ALTER TABLE `video_time`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
